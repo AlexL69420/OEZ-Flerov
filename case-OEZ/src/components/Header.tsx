09e5dde8b40fc77765dae87159d067f746983f37
alexander <alexleto24@gmail.com>
2024-11-16 14:52:04 +0300

    msg

--- case-OEZ/src/components/Header.tsx
+++ case-OEZ/src/components/Header.tsx
@@ -9,23 +9,32 @@ import { VscAccount } from "react-icons/vsc";
 
 export default function Header() {
   return (
-    <main className="bg-tmpk-blue flex h-16 w-full items-center justify-between gap-2 bg-blue-700 dark:bg-slate-800">
-      <div className="flex h-12 flex-wrap items-center gap-1">
-        <Avatar
-          className={"w-12"}
-          img="src\assets\logo.svg"
-          alt="logo"
-          rounded
-        />
-        <h1 className="text-2xl text-white ">Флерова</h1>
-      </div>
+    <main className="flex h-16 w-full items-center justify-around bg-blue-700 dark:bg-slate-800">
+      <div className="flex w-11/12 justify-between gap-2">
+        <div className="flex h-12 flex-wrap items-center gap-1">
+          <Avatar
+            className={"w-12"}
+            img="src\assets\logo.svg"
+            alt="logo"
+            rounded
+          />
+          <h1 className="text-2xl text-white ">Флерова</h1>
+        </div>
 
-      <Button className="bg-inherit dark:bg-inherit">
-        <Link to="/options">
-          <Avatar img={VscAccount} rounded />
-        </Link>
-      </Button>
-      <DarkThemeToggle />
+        <div className="flex w-1/6 flex-row items-center justify-around">
+          <div className="flex flex-row gap-4">
+            <Button
+              color="blue"
+              className="max-h-12 max-w-12 items-center rounded-full bg-blue-700 p-1 text-white dark:bg-slate-800 dark:hover:bg-slate-700"
+            >
+              <Link to="/options">
+                <Avatar img={VscAccount} rounded />
+              </Link>
+            </Button>
+            <DarkThemeToggle className="flex size-12 items-center justify-around rounded-full  text-white  hover:bg-blue-600 dark:text-slate-500 dark:hover:bg-slate-700" />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- case-OEZ/src/components/MainContent.tsx
+++ case-OEZ/src/components/MainContent.tsx
@@ -1,17 +1,23 @@
 "use client";
 
-import { Label, TextInput } from "flowbite-react";
+import { Label, TextInput, Button } from "flowbite-react";
 
 export function MainContent() {
   return (
     <main className="flex min-h-screen w-screen flex-col  gap-2 bg-white p-2 text-black dark:bg-slate-600 dark:text-white">
-      <div className="flex max-w-md flex-col gap-4">
-        <div>
+      <div className="flex flex-col gap-4">
+        <div className="flex max-w-screen-lg flex-col">
           <div className="mb-2 block">
-            <Label htmlFor="base" value="Base input" />
+            <Label htmlFor="base" value="Введите симптомы" />
           </div>
           <TextInput id="base" type="text" sizing="md" />
         </div>
+        <Button
+          color="blue"
+          className="max-w-36 hover:bg-blue-500 dark:bg-black dark:hover:bg-slate-700"
+        >
+          Отправить
+        </Button>
       </div>
     </main>
   );
