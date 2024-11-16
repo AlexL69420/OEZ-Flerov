d5c439c0c3436584e88097fc2a56214368b4d1ec
alexander <alexleto24@gmail.com>
2024-11-16 16:45:49 +0300

    msg

--- case-OEZ/src/components/Header.tsx
+++ case-OEZ/src/components/Header.tsx
@@ -6,6 +6,7 @@ import { DarkThemeToggle } from "flowbite-react";
 ("use client");
 
 import { VscAccount } from "react-icons/vsc";
+import { FiActivity } from "react-icons/fi";
 
 export default function Header() {
   return (
@@ -13,25 +14,27 @@ export default function Header() {
       <div className="flex w-11/12 justify-between gap-2">
         <div className="flex h-12 flex-wrap items-center gap-1">
           <Avatar
-            className={"w-12"}
-            img="src\assets\logo.svg"
+            className={"w-12 text-white dark:text-red-800"}
+            img={FiActivity}
             alt="logo"
             rounded
           />
-          <h1 className="text-2xl text-white ">Флерова</h1>
+          <h1 className="font-mono text-3xl italic  text-white dark:text-red-800">
+            Флерова
+          </h1>
         </div>
 
         <div className="flex w-1/6 flex-row items-center justify-around">
           <div className="flex flex-row gap-4">
             <Button
               color="blue"
-              className="max-h-12 max-w-12 items-center rounded-full bg-blue-700 p-1 text-white dark:bg-slate-800 dark:hover:bg-slate-700"
+              className="max-h-12 max-w-12 items-center rounded-full bg-blue-700 p-1 text-white dark:bg-slate-800 dark:text-slate-500 dark:hover:bg-slate-700"
             >
               <Link to="/options">
                 <Avatar img={VscAccount} rounded />
               </Link>
             </Button>
-            <DarkThemeToggle className="flex size-12 items-center justify-around rounded-full  text-white  hover:bg-blue-600 dark:text-slate-500 dark:hover:bg-slate-700" />
+            <DarkThemeToggle className="flex size-12 items-center justify-around rounded-full border-2 border-white text-white  hover:bg-blue-600  dark:border-slate-500 dark:text-slate-500 dark:hover:bg-slate-700" />
           </div>
         </div>
       </div>
--- case-OEZ/src/components/MainContent.tsx
+++ case-OEZ/src/components/MainContent.tsx
@@ -4,13 +4,16 @@ import { Label, TextInput, Button } from "flowbite-react";
 
 export function MainContent() {
   return (
-    <main className="flex min-h-screen w-screen flex-col  gap-2 bg-white p-2 text-black dark:bg-slate-600 dark:text-white">
+    <main className="flex min-h-screen w-screen flex-col  gap-2 bg-white p-3 text-black dark:bg-slate-600 dark:text-white">
       <div className="flex flex-col gap-4">
-        <div className="flex max-w-screen-lg flex-col">
+        <div className="flex w-1/2 flex-col">
           <div className="mb-2 block">
             <Label htmlFor="base" value="Введите симптомы" />
           </div>
-          <TextInput id="base" type="text" sizing="md" />
+          <div className="flex flex-row items-center gap-2">
+            <TextInput id="base" type="text" sizing="md" />
+            <Button>?</Button>
+          </div>
         </div>
         <Button
           color="blue"
